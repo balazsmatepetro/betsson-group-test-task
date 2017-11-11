8ef2b927eaa4d50b08eac6cd6043df1016fea8b9
Balázs Máté Petró <petrobalazsmate@gmail.com>
2017-11-11 23:18:15 +0100

    some improvements

--- spec/category/category.entity.spec.js
+++ spec/category/category.entity.spec.js
@@ -1,23 +1,24 @@
 import Category from '../../src/category/category.entity';
 
 describe('Category entity creation', () => {
+    const id = 1;
+    const title = 'Category 1';
+
     describe('should fail', () => {
         it('when the ID is not a number', () => {
-            expect(() => new Category('ID')).toThrowError('The ID must be a number!');
+            expect(() => new Category('id')).toThrowError('The ID must be a number!');
         });
 
         it('when the title is not a string', () => {
-            expect(() => new Category(1, 1)).toThrowError('The title must be a string!');
+            expect(() => new Category(id, true)).toThrowError('The title must be a string!');
         });
 
         it('when the title is an empty string', () => {
-            expect(() => new Category(1, '')).toThrowError('The title cannot be an empty string!');
+            expect(() => new Category(id, '')).toThrowError('The title cannot be an empty string!');
         });
     });
 
     describe('should pass', () => {
-        const id = 1;
-        const title = 'Category 1';
         let category = undefined;
 
         beforeEach(() => {
@@ -32,11 +33,11 @@ describe('Category entity creation', () => {
             expect(category.title).toEqual(title);
         });
 
-        it('when the \'isActive\' value is not provided', () => {
+        it('when the isActive value is not provided', () => {
             expect(category.isActive).toEqual(true);
         });
 
-        it('when the \'isActive\' value is provided', () => {
+        it('when the isActive value is provided', () => {
             const category = new Category(id, title, false);
 
             expect(category.isActive).toEqual(false);
