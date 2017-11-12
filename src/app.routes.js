94116bca194c6989cbb991f3df0d5949fe734a6f
Balázs Máté Petró <petrobalazsmate@gmail.com>
2017-11-12 20:53:28 +0100

    updated app routing

--- src/app.routes.js
+++ src/app.routes.js
@@ -1,8 +1,70 @@
 export default function appRouting($urlRouterProvider, $stateProvider) {
     $stateProvider
-        .state('featured', {
+        .state('app', {
+            template: '<category-tree data-categories="categoryTree"></category-tree><div ui-view></div>',
+            controller: ['$scope', 'categoryTreeResolver', ($scope, categoryTreeResolver) => {
+                $scope.categoryTree = categoryTreeResolver.categories;
+            }],
+            resolve: {
+                categoryTreeResolver: [
+                    '$q',
+                    'categoryService',
+                    'categoryTreeService',
+                    ($q, categoryService, categoryTreeService) => {
+                        return categoryService
+                            .findAll()
+                            .then(onSuccess)
+                            .catch(onError);
+
+                        function onError() {
+                            return $q.reject('The categories couldn\'t be located!');
+                        }
+
+                        function onSuccess(categories) {
+                            return $q.resolve({
+                                categories: categoryTreeService.build(categories)
+                            });
+                        }
+                    }
+                ]
+            }
+        })
+        .state('app.featured', {
+            url: '/',
+            component: 'featuredPostList',
+            resolve: {
+                posts: [
+                    'featuredPostListService',
+                    (featuredPostListService) => featuredPostListService.findAll()
+                ]
+            }
+        })
+        .state('app.category', {
             component: 'postList',
-            url: '/'
+            url: '/categories/{id}/posts',
+            resolve: {
+                category: [
+                    '$stateParams',
+                    'categoryService',
+                    ($stateParams, categoryService) => categoryService.findById($stateParams.id)
+                ],
+                posts: [
+                    '$stateParams',
+                    'postService',
+                    ($stateParams, postService) => postService.findByCategoryId($stateParams.id)
+                ]
+            }
+        })
+        .state('app.post', {
+            component: 'postDetailed',
+            url: '/posts/{id}/show',
+            resolve: {
+                post: [
+                    '$stateParams',
+                    'postService',
+                    ($stateParams, postService) => postService.findById($stateParams.id)
+                ]
+            }
         });
 
     $urlRouterProvider.otherwise('/');
