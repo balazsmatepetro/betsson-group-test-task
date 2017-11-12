3d3c6dffd73069d84924bf434a81f98f34ff5cc4
Balázs Máté Petró <petrobalazsmate@gmail.com>
2017-11-12 21:18:29 +0100

    updated app module

--- src/app.module.js
+++ src/app.module.js
@@ -1,16 +1,34 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import appRouting from './app.routes';
+import categoryModule from './category/category.module';
+import categoryDetailedModule from './category-detailed/category-detailed.module';
+import categoryTreeModule from './category-tree/category-tree.module';
+import featuredPostListModule from './featured-post-list/featured-post-list.module';
+import postModule from './post/post.module';
+import postDetailedModule from './post-detailed/post-detailed.module';
 import postListModule from './post-list/post-list.module';
+import postListItemModule from './post-list-item/post-list-item.module';
 
 const moduleName = 'app';
 
 angular
     .module(moduleName, [
         uiRouter,
-        postListModule
+        categoryModule,
+        categoryDetailedModule,
+        categoryTreeModule,
+        featuredPostListModule,
+        postModule,
+        postDetailedModule,
+        postListModule,
+        postListItemModule
     ])
-    .config(['$urlRouterProvider', '$stateProvider', appRouting]);
+    .config([
+        '$urlRouterProvider',
+        '$stateProvider',
+        appRouting
+    ]);
 
 angular
     .element(document)
