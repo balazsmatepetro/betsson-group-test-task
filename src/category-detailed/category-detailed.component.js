ca07d1eb6c96a4869ce8e908f6fd8e70815340a3
Balázs Máté Petró <petrobalazsmate@gmail.com>
2017-11-12 21:17:43 +0100

    added category detailed component and module

---
+++ src/category-detailed/category-detailed.component.js
@@ -0,0 +1,8 @@
+export default {
+    bindings: {
+        category: '<',
+        posts: '<'
+    },
+    controllerAs: 'vm',
+    templateUrl: 'src/category-detailed/category-detailed.component.html'
+};
---
+++ src/category-detailed/category-detailed.module.js
@@ -0,0 +1,7 @@
+import angular from 'angular';
+import component from './category-detailed.component';
+
+export default angular
+    .module('app.category-detailed', [])
+    .component('categoryDetailed', component)
+    .name;
